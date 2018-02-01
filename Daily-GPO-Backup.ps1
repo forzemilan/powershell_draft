7918929e0452e3be8980880d331afeefd06edede
forzemilan <wh410183@163.com>
2018-02-01 13:14:04 +0800

    archive GPO backup files

--- Daily-GPO-Backup.ps1
+++ Daily-GPO-Backup.ps1
@@ -11,3 +11,10 @@ new-item -ItemType Directory -Path $path
 #step 2:
 #backup GPO everyday and store in above path
 backup-gpo -All -Path $path -Comment $foldername
+
+#step 3: archive backup files.
+$log_folder = ' "E:\GPO-Backup\"+$foldername'
+Compress-Archive -Path  $log_folder -CompressionLevel Optimal -DestinationPath $log_folder
+
+#step 4: remove backup files.
+Remove-Item -Path $log_folder -Recurse
