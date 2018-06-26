62695adbddbbd11cde92bf30dda18f450ae6b5cc
spookw <spookw@oc5760625243.ibm.com>
2018-06-26 12:10:04 +0800

    add 2.10 Domain Group Policy Checks

--- AD_health_check_report.ps1
+++ AD_health_check_report.ps1
@@ -33,11 +33,19 @@ Select @{Label = "useraccountcontrol";Expression = {if (($_.useraccountcontrol -
     Resolve-DnsName -Name $DomainControllers -Type A
 
 #2.5  Domain Controller Advertising
+    #This check verifies that domain controllers are advertising their capabilities. The following service advertisements are tested:
+
+        #Advertising as a DC and having a Directory Service (DC)
+        #Advertising as having a writable directory (WD)
+        #Advertising as a Key Distribution Centre (KDC)
+        #Advertising as a time server (TS)
+        #Advertising as a Global Catalog (GC) (Note: Not all DCs are GCs)
 
     $svcs = "kdc","Dfs"  #please adjust the services name 
     Get-Service -name $svcs -ComputerName $DomainControllers | Sort Machinename | Format-Table -group @{Name="Computername";Expression={$_.Machinename.toUpper()}} -Property Name,Displayname,Status
 
 #2.6  Domain Controller Default Shares
+    #The existence of the Sysvol and Netlogon shares is checked on each domain controller. These shared directories are required for group policy processing and logon script execution.
     Get-WmiObject -Class Win32_Share -ComputerName $DomainControllers -credential $cred | ?{$_.description -eq "Default share"} | sort ComputerName | Format-Table -Group @{Name="Computername";Expression={$_.computername.toupper()}} -Property name,path,description 
 
 #2.7  Domain DFS Referral Configuration
@@ -45,14 +53,28 @@ Select @{Label = "useraccountcontrol";Expression = {if (($_.useraccountcontrol -
 
 #2.8  Domain Functional Level
      # Get Domain Functional Level 
+     # The domain functional level is queried to ensure that it is set correctly. The Active Directory domain functional level should be set to the highest possible functional level as soon as possible.
      Get-ADDomain | fl Name,DomainMode
      # Get Forest Functional Level
      Get-ADForest | fl Name,ForestMode 
 #2.9  Domain FSMO Checks
+    # The servers hosting domain FSMO roles are determined. It is best practice to locate the PDCE on the same server as the RID master. The Infrastructure Master must be hosted on a non Global Catalog DC unless all DCs in the domain are Global Catalogs.
     NETDOM /QUERY FSMO
 
 #2.10 Domain Group Policy Checks
     Import-Module grouppolicy
+    #i. Default Policies Check
+    #This check ensures that the "Default Domain Policy" and "Default Domain Controllers Policy" have not been removed from the domain. These policies must be present for some applications to install properly.
+    get-GPO -name "Default Domain Policy"
+    get-GPO -name "Default Domain Controllers Policy"
+
+    #ii. Orphaned Policies Check
+    #This check searches for group policies which have been removed from the AD but not removed from SYSVOL. Orphaned policies should be removed from the SYSVOL to reduce the overall size of the SYSVOL.
+    # Policies compared with PDC
+    $PDC_sysvol_path = "\\PDC\sysvol\AIA.BIZ\Policies"
+    $gpo_sysvol = Get-ChildItem $PDC_sysvol_path | ?{$_.Name -match "{*}"} | ForEach-Object -Process {$_.Name.ToString().Substring(1,36)} 
+    $gpo_list = Get-GPO -all | ForEach-Object -Process {$_.id.tostring()}
+    Compare-Object  $gpo_sysvol $gpo_list
 
     #iii. Unlinked Policies Check  result issue   this command will cost long time
     Get-GPO -All | 
@@ -62,10 +84,46 @@ Select @{Label = "useraccountcontrol";Expression = {if (($_.useraccountcontrol -
               Write-Host $_.DisplayName
              }
       }
+
+    #iv. Mismatched Versions Check
+    #This check searches for group policies whose SYSVOL versions do not match those in the AD. Mismatched versions indicate a problem where policies have not been committed to the SYSVOL.
+    #The following policies were compared with PDC
+    $gpo_guid = Get-GPO -all | ForEach-Object -Process {$_.id.tostring()}
+    foreach ($i in $gpo_guid) {
+        
+        if ((Get-GPO -Guid $i).user.dsversion -ne (Get-GPO -Guid $i).user.sysvolversion)  {
+            get-gpo -Guid $i | select name
+        } else{
+            write-host pass -ForegroundColor Green
+        }
+
+
+        if ((Get-GPO -Guid $i).computer.dsversion -ne (Get-GPO -Guid $i).computer.sysvolversion)  {
+            get-gpo -Guid $i | select name
+        } else{
+            write-host pass -ForegroundColor Green
+        }
+
+    }
+
+    #The following changes should be made:
+    #The orphaned policies listed, should be removed from the SYSVOL, as they are no longer present in the Directory Service. WARNING: Modifying the SYSVOL incorrectly can have a significant impact on the Active Directory. Deletions can replicate very quickly and restoring the data can take a significant amount of time. Take extreme caution when removing these policies.
+    #The unlinked policies listed, should be removed using GPMC (if they are no longer required), to reduce the overall size of the SYSVOL
+
 #2.11 Inactive Accounts Check
+
+    #This check ensures that user accounts are disabled/removed when they are no longer required.
+    #This check ensures that computer accounts are disabled/removed when they are no longer required.
+    
     Import-Module activedirectory
     Search-ADAccount -accountinactive -usersonly | ft name,sid,lastlogondate
     Search-ADAccount -accountinactive -computersonly | ft distinguishedname,lastlogondate
+
+    
+    #The following changes should be made:
+
+    #User accounts which are no longer in use should be removed or disabled
+    #Computer accounts which are no longer in use should be removed or disabled
 
 #2.12 Domain Controller OS Version
 
